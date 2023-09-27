ff5fc3e380783c2ec5989ebcd35bbe0b1ede3a33
veerasrikanth <veerasrikanthv@gmail.com>
2023-09-26 22:24:29 -0400

    my first commit

---
+++ apps/host/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mono-nx-dyn-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'host';
+}
